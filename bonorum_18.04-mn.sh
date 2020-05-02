e13844ca5b8f94e0ac21efa43c46adc2c2e0c1df
bonoproject <60004952+bonoproject@users.noreply.github.com>
2020-05-02 23:45:49 +0530

    Port Update

--- bonorum_18.04-mn.sh
+++ bonorum_18.04-mn.sh
@@ -15,8 +15,8 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 #TCP port
-PORT=11915
-RPC=11914
+PORT=1619
+RPC=1618
 
 #Clear keyboard input buffer
 function clear_stdin { while read -r -t 0; do read -r; done; }
